208fbf774819f1c9d603adea2e686a6c443143aa
abdullah-a8 <github@corestack.simplelogin.com>
2024-12-03 11:56:31 +0500

    Updated Mongoose Depreciated Modules

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,21 +1,14 @@
-/* This module will hold our connection to 
-   our mongo server through the Mongoose API.
-   We will access the connection in our express server. */
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-/* Connnect to our database */
 // Get the URI of the local database, or the one specified on deployment.
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/steamTracker';
 
-mongoose.connect(mongoURI, {
-   useNewUrlParser: true,
-   useUnifiedTopology: true,
-})
- .then(() => {
-     console.log('MongoDB connected...');
- })
- .catch((err) => {
-     console.error('MongoDB connection error:', err);
- });
+mongoose.connect(mongoURI)
+  .then(() => {
+      console.log('MongoDB connected...');
+  })
+  .catch((err) => {
+      console.error('MongoDB connection error:', err);
+  });
 
- module.exports = { mongoose };
+module.exports = { mongoose };
